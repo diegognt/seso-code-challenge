154da05b125977334b0300624ba77c135fa7f240
Diego Navarro <diego.nava07@gmail.com>
2022-04-02 17:56:03 -0500

    Solution for the challenges.
    Let's use a Heapsort algorithm over the merged array.

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,34 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
+const heapify = require('../lib/heapify');
+
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+  return new Promise(async (resolve, reject) => {
+    let mergedLogs = [];
+    for (const logSource of logSources) {
+      while (!logSource.drained) {
+        const entry = await logSource.pop();
+        if (entry) {
+          mergedLogs.push(entry);
+        }
+      }
+    }
+  
+    const mergedLogsLength = mergedLogs.length;
+  
+    // Build heap (rearrange array)
+    for (let i = parseInt(mergedLogsLength / 2 - 1); i >= 0; i--)
+      heapify(mergedLogs, mergedLogsLength, i);
+  
+    // One by one extract an element from heap
+    for (let i = mergedLogsLength - 1; i >= 0; i--) {
+      printer.print(mergedLogs[0]);
+      [mergedLogs[0], mergedLogs[i]] = [mergedLogs[i], mergedLogs[0]];
+      heapify(mergedLogs, i, 0);
+    }
+  
+    printer.done();
+    return console.log("Sync sort complete.");
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,35 @@
 "use strict";
 
 // Print all entries, across all of the sources, in chronological order.
+// Heapsort implementation.
+
+const heapify = require('../lib/heapify');
 
 module.exports = (logSources, printer) => {
+  let mergedLogs = [];
+  for (const logSource of logSources) {
+    while (!logSource.drained) {
+      const entry = logSource.pop();
+      if (entry) {
+        mergedLogs.push(entry);
+      }
+    }
+  }
+
+  const mergedLogsLength = mergedLogs.length;
+
+  // Build heap (rearrange array)
+  for (let i = parseInt(mergedLogsLength / 2 - 1); i >= 0; i--)
+    heapify(mergedLogs, mergedLogsLength, i);
+
+  // One by one extract an element from heap
+  for (let i = mergedLogsLength - 1; i >= 0; i--) {
+    printer.print(mergedLogs[0]);
+    [mergedLogs[0], mergedLogs[i]] = [mergedLogs[i], mergedLogs[0]];
+    heapify(mergedLogs, i, 0);
+  }
+
+  printer.done();
   return console.log("Sync sort complete.");
 };
+
